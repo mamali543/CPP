4caa3cab2238929596520de64a6ff355f467a2c4
Mohammed reda Amali <mamali@student.1337.ma>
2021-11-24 19:07:07 +0100

    day01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,15 +3,31 @@
 int main()
 {
     string p;
+    string t;
     phonebook phone;
 
+    // getline(cin, t);
+    // cout << t << endl;
     while (1)
     {
         cout << "$cmd>" ; 
         cin >> p;
         if (p.compare("ADD") == 0)
         {
-            cout << "First name: " << endl;
+            cout << "First name: ";
+            cin.ignore();
+            getline(cin, t);
+            phone.setfs(t);
+            cout << "Last name: ";
+            getline(cin, t);
+            phone.setln(t);
+            cout << "Phone number: ";
+            getline(cin, t);
+            phone.setpn(t);
+            cout << "Darkest secret: ";
+            getline(cin, t);
+            phone.setds(t);
+            cout << phone.getfs() << endl << phone.getln() << endl << phone.getpn() << endl << phone.getds() << endl;   
         }
         else if (p.compare("SEARCH") == 0)
             cout << p << endl;
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -12,8 +12,29 @@ class phonebook{
 		string	phone_number;
 		string  dark_secret;   
 	public:
+		void setpn(string pn){
+			phone_number = pn;
+		}
 		void setfs(string fs){
 			f_name = fs;
+		}
+		void setln(string ln){
+			l_name = ln;
+		}
+		void setds(string ds){
+			dark_secret = ds;
+		}
+		string getfs(){
+			return (f_name);
+		}
+		string getln(){
+			return (l_name);
+		}
+		string getpn(){
+			return (phone_number);
+		}
+		string getds(){
+			return (dark_secret);
 		}
 };
 
