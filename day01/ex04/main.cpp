ed620461463d914213205278727d1e01c468c13a
Mohammed reda Amali <mamali@student.1337.ma>
2021-12-14 01:30:28 +0100

    ex05

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -4,19 +4,28 @@
 
 int     main(int    argc, char**    argv)
 {
-    int     i;
     std::string text;
 
-    i = 0;
 
     if (argc != 4)
+    {
         std::cout << "invalid args" << std::endl;
+        return (0);
+    }
     std::ifstream file(argv[1]);
     if (file.is_open() == 0)
-        std::cout << "ther's no such file : " << argv[1] << std::endl;
+    {
+            std::cout << "ther's no such file : " << argv[1] << std::endl;
+            return (0);
+    }
     std::string ffile = argv[1];
     std::string s1 = argv[2];
     std::string s2 = argv[3];
+    if (s1.empty() || s2.empty())
+    {
+        std::cout << "invalid args" << std::endl;
+        return (0);     
+    }
     ffile = ffile + ".replace";
     std::ofstream Myfile(ffile);
     std::ifstream Myreadfile(argv[1]);
@@ -26,21 +35,18 @@ int     main(int    argc, char**    argv)
         int start = 0;
         std::string a = "";
         std::string b = "";
-        if (i != 0)
-            Myfile << '\n';
         found = text.find(s1, 0);
         while (found != std::string::npos)
         {
             a = text.substr(start, found - start);
-            std::cout << "a>>" << a << "\n----------\n";
             b = b + a + s2;
-            std::cout << "b>>" << b << "\n";
             start = found + s1.size();
             found = text.find(s1, found + s1.size());
         }
         a = text.substr(start, text.size());
         Myfile << b + a;
-        i++;
+        if (!Myreadfile.eof())
+            Myfile << '\n';
     }
     Myreadfile.close();
     Myfile.close();
